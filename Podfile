f908909bd5d571f8d968f08d33ec9b4d05a10b72
Alex <therebeloper@gmail.com>
2019-03-02 09:58:27 +0200

    first commit

---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment the next line to define a global platform for your project
+  platform :ios, '12.0'
+
+target 'UIImagePickerControllerExample' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for UIImagePickerControllerExample
+pod 'TinyConstraints'
+
+end
--- UIImagePickerControllerExample/RootViewController.swift
+++ UIImagePickerControllerExample/RootViewController.swift
@@ -6,15 +6,59 @@
 //  Copyright © 2019 Alex Nagy. All rights reserved.
 //
 
-import UIKit
+import TinyConstraints
 
 class RootViewController: UIViewController {
+    
+    let profileImageViewWidth: CGFloat = 100
+    
+    lazy var profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "DefaultProfileImage").withRenderingMode(.alwaysOriginal)
+        iv.contentMode = .scaleAspectFill
+        iv.layer.cornerRadius = profileImageViewWidth / 2
+        iv.layer.masksToBounds = true
+        return iv
+    }()
+    
+    lazy var profileImageButton: UIButton = {
+        var button = UIButton(type: .system)
+        button.backgroundColor = .clear
+        button.layer.cornerRadius = profileImageViewWidth / 2
+        button.layer.masksToBounds = true
+        button.addTarget(self, action: #selector(profileImageButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc fileprivate func profileImageButtonTapped() {
+        print("Tapped button")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupViews()
     }
 
+    fileprivate func setupViews() {
+        view.backgroundColor = .white
+        addViews()
+        constrainViews()
+    }
+    
+    fileprivate func addViews() {
+        view.addSubview(profileImageView)
+        view.addSubview(profileImageButton)
+    }
+    
+    fileprivate func constrainViews() {
+        profileImageView.topToSuperview(offset: 36, usingSafeArea: true)
+        profileImageView.centerXToSuperview()
+        profileImageView.width(profileImageViewWidth)
+        profileImageView.height(profileImageViewWidth)
+        
+        profileImageButton.edges(to: profileImageView)
+    }
 
 }
 
