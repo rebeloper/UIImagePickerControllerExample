433fbf0945ce65bda3d636bdca600d3e6b3df4f9
Alex <therebeloper@gmail.com>
2019-03-02 09:44:08 +0200

    Initial Commit

---
+++ UIImagePickerControllerExample/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  UIImagePickerControllerExample
+//
+//  Created by Alex Nagy on 02/03/2019.
+//  Copyright © 2019 Alex Nagy. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
